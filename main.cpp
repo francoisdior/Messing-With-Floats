58fa44899f98de89ee97292cd62651bf0ad32911
francoisdior <78714839+francoisdior@users.noreply.github.com>
2021-02-18 23:09:06 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,7 +5,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printBinary( unsigned long fnum )
+void printBinary_LayoutIEEE754( unsigned long fnum )
+{
+    unsigned long m=0x8000000000000000;
+    int i=0, j=0;
+    int strlen=64+2+2; // (binary characters) + spaces + ('b' + '\0')
+    char out[ strlen ];
+    int offsets[] = { 1, 11, 9999 };
+    int currOff = offsets[j++];
+
+    while(m) {
+        if( m & fnum)
+            out[i] = '1';
+        else
+            out[i] = '0';
+
+        m >>= 1;
+        i++;
+
+        if( i == currOff ) {
+            out[i++] = ' ';
+            currOff = offsets[j++]+i;
+        }
+    }
+
+    out[ strlen-2 ] = 'b';
+    out[ strlen-1 ] = 0;
+
+    printf("%s\n", out);
+}
+
+void printBinary_GroupsOf4( unsigned long fnum )
 {
     unsigned long m=0x8000000000000000;
     int i=0, j=0;
@@ -30,7 +60,7 @@ void printBinary( unsigned long fnum )
     out[ strlen-3 ] = 'b';
     out[ strlen-1 ] = 0;
 
-    printf("%s", out);
+    printf("%s\n", out);
 }
 
 int main()
@@ -45,9 +75,10 @@ int main()
 
         toLong = *((unsigned long*) &dec);
         printf("%f\n%lXh\n", dec, toLong);
-        printBinary(toLong);
+        printBinary_GroupsOf4(toLong);
+        printBinary_LayoutIEEE754(toLong);
 
-        printf("\n\n");
+        printf("\n");
         printf("Continue? ");
         scanf("%s", str);
         printf("\n");
